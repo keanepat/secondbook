420b5f9a33e213fe3e50031c3ad666c8357945e0
Patrick Keane <keanepat@Patricks-MacBook-Air.local>
2014-06-04 23:51:28 -0400

    refreshing my rails, don't like scaffold...to easy

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Secondbook::Application.config.secret_key_base = 'cf5002360de71539064f949f47f221aebc7ca64ac1d399269edf3c28f0bd6b47e98c43efc2bd2e9d07a29b29a01078cf0a50c52667c4e9f25c8493795c530945'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Secondbook::Application.config.session_store :cookie_store, key: '_secondbook_session'
